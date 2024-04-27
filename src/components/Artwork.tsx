0bfac09cb64c78e8ba0e72118e29424aa6ee5cd0
tombyrn <thomasbyrnes02@gmail.com>
2024-04-27 19:08:13 -0400

    frontend fixes

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -1,9 +1,12 @@
 import { Button, Card, CardBody, CardHeader, Typography } from "@material-tailwind/react";
 import { useContext, useEffect, useState } from "react";
 import Image from 'next/image';
-import { UserCollectionContext, UserCollectionProvider, capacity } from "@/context/UserCollectionContext";
+import { UserCollectionContext, capacity } from "@/context/UserCollectionContext";
+import { useRouter } from "next/router";
 
-export default function Artwork({id}: {id: string}) {
+export default function Artwork({id}: {id: number}) {
+    const router = useRouter();
+
     // array of objects id strings stored in global context
     /* @ts-ignore */
     const {userCollection, setUserCollection} = useContext(UserCollectionContext);
@@ -34,42 +37,69 @@ export default function Artwork({id}: {id: string}) {
         }
         // remove id from user collection if it's present
         else if(userCollection.includes(id)){
-            setUserCollection(userCollection.filter((item: string) => item !== id))
+            setUserCollection(userCollection.filter((item: number) => item !== id))
             setPresent(false);
         }
     }
 
-    console.log('artworkData', data)
+    // console.log('artworkData', data)
     return (
         <div> 
             {!loading && ( <>
                 {/* @ts-ignore */}
-                <Card className="flex flex-row justify-center m-12">
+                <Card className={`w-full flex flex-row justify-center m-6 p-1 border-opacity-40 border-4  ${data.isHighlight && "border-yellow-700"}`}>
                     {/* @ts-ignore */}
-                    <CardHeader className="w-2/5 flex justify-center m-0 shrink-0 rounded-r-none">
-                        <Image src={data.primaryImage || "/n-a.jpg"} alt={data.title || "no alt text available"} width={300} height={300} className="h-full w-full object-cover"/>
+                    <CardHeader className="w-2/5 flex justify-center m-0 shrink-0 rounded-r-none" onClick={() => router.push(`/artwork/${id}`)}>
+                        <Image src={data.primaryImage || "/n-a.jpg"} alt={data.title || "no alt text available"} fill={true} style={{objectFit: "contain"}} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
                     </CardHeader>
 
                     {/* @ts-ignore */}
-                    <CardBody className="w-3/5 p-10 flex flex-col justify-between items-center text-ellipsis">
-                        {/* @ts-ignore */}
-                        <Typography variant="h3" className="m-2">
-                            {data.title}
-                        </Typography>
+                    <CardBody className="w-3/5 p-10 flex flex-col justify-between items-center">
+                        <div className="w-full flex flex-row justify-center items-center">
+                            {/* @ts-ignore */}
+                            <Typography variant="small" className="m-2">
+                                Title
+                            </Typography>
+                            
+                            {/* @ts-ignore */}
+                            <Typography variant="h3" className="m-2">
+                                {data.title || "N/A"}
+                            </Typography>
+                        </div>
+                        <div className="w-full flex flex-row justify-center items-center">
+                            {/* @ts-ignore */}
+                            <Typography variant="small" className="m-2">
+                                Artist Name
+                            </Typography>
+                            
+                            {/* @ts-ignore */}
+                            <Typography variant="h5" className="m-2">
+                                {data.artistDisplayName || "N/A"}
+                            </Typography>
+                        </div>
+                        <div className="w-full flex flex-row justify-center items-center">
+                            {/* @ts-ignore */}
+                            <Typography variant="small" className="m-2">
+                                Department
+                            </Typography>
+                        
+                            {/* @ts-ignore */}
+                            <Typography variant="h6" className="m-2">
+                                {data.department || "N/A"}
+                            </Typography>
+                        </div>
+                        <div className="w-full flex flex-row justify-center items-center">
+                            {/* @ts-ignore */}
+                            <Typography variant="small" className="m-2">
+                                Medium
+                            </Typography>
+                        
+                            {/* @ts-ignore */}
+                            <Typography variant="h6" className="m-2">
+                                {data.medium || "N/A"}
+                            </Typography>
+                        </div>
 
-                        {/* @ts-ignore */}
-                        <Typography variant="h5" className="m-2">
-                            {data.artistDisplayName}
-                        </Typography>
-
-                        {/* @ts-ignore */}
-                        <Typography variant="subtitle" className="m-2">
-                            {data.department}
-                        </Typography>
-                        {/* @ts-ignore */}
-                        <Typography variant="subtitle" className="m-2">
-                            {data.medium}
-                        </Typography>
 
                         {/* @ts-ignore */}
                         <Button size="lg" ripple={true} onClick={toggleCollection} disabled={userCollection.length >= capacity && !userCollection.includes(id)}>{!present ? "Add to Collection" : "Remove from Collection"}</Button>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({children}: {children: ReactNode}) {
     const router = useRouter();
     return (
         <div className="m-0 p-0 w-screen h-screen flex flex-col justify-center items-center">
-            <div className="pl-10 w-full h-1/6 flex items-center border-4 border-transparent border-b-red-600">
+            <div className="sticky top-0 bg-white z-10 pl-10 pb-3 w-full h-fit flex items-center border-4 border-transparent border-b-red-600">
                 <MetLogo/>
                 <NewspaperIcon title="Random Collection" className="ml-11 mt-3 w-7 h-7 text-black hover:text-gray-500" onClick={() => router.push(`/page/1`)}/>
                 <CircleStackIcon title="Your Collection" className="ml-3 mt-3 w-7 h-7 text-black hover:text-gray-500" onClick={() => router.push(`/collection`)}/>
